83ae03814c488a0d66e534c226d267a0509b555d
katzchang <katzchang@gmail.com>
2013-02-01 03:52:53 +0900

    refactor

--- gauche/unit/test-suite.scm
+++ gauche/unit/test-suite.scm
@@ -4,32 +4,32 @@
 	  run-test)
 
   (use gauche.sequence)
-
   (define test
     (lambda mods (exit (run-suite mods))))
-
 
   (define (flatmap fn ls)
     (apply append (map fn ls)))
   
+  (define (collect item ls)
+    (filter (^(x) (eq? x item)) ls))
   (define (run-suite mods)
     (let1 result (flatmap run-test mods)
 	  (let ((total (length result))
-		(pass (length (filter (^(x) (eq? x 'ok)) result)))
-		(err (length (filter (^(x) (eq? x 'err)) result)))
-		(ret (if (any (^(e) (eq? e 'err)) result) #f #t)))
+		(pass (length (collect 'ok result)))
+		(err (length (collect 'err result))))
 	    (print "total: " total ", pass: " pass ", error: " err)
-	    (print (if ret 'ok 'err))
+	    (print (if (= err 0) 'ok 'err))
 	    err)))
-  
+
+  (define env (interaction-environment))
   (define (run-test mod)
     (let1 result
-	  (eval `(use ,mod) (interaction-environment))
+	  (eval `(use ,mod) env)
 	  (let1 exports (module-exports (find-module mod))
 		(map (lambda (s)
 		       (print s "...")
 		       (guard (e (else (begin (print e) 'err)))
 			      (begin
-				(eval `(,s) (interaction-environment))
+				(eval `(,s) env)
 				'ok)))
 		     exports)))))
