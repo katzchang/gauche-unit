c5d03ce638a6fc80917f1d85387c47ea7554e95d
katzchang <katzchang@gmail.com>
2013-01-31 23:21:58 +0900

    ...

--- t/hello-suite.scm
+++ t/hello-suite.scm
@@ -1,17 +1,12 @@
 (use gauche.unit)
 
-(define (test description . procs)
-  (print procs))
-
-#;(test "hello"
- (assert 1 (is 1))
- (assert "xhoge" (is "hoge")))
-
 (define-module t.hello-suite
   (export hello-test)
+
   (use gauche.unit)
   (define (test description . procs)
     eval procs)
+
   (define (hello-test) (test
    (assert 1 (is 1))
-   (assert "xhoge" (is "hoge")))))
+   (assert "hoge" (is "hoge")))))
