18144209337d8e53d71ff5129f2f195bcd551905
katzchang <katzchang@gmail.com>
2013-02-01 03:14:30 +0900

    refactor

--- t/run_suite.scm
+++ t/run_suite.scm
@@ -1,8 +1,7 @@
 (use gauche.sequence)
 
 (define (main args)
-  (exit (let1 ret (run-suite '(t.hello-suite))
-	      (if ret 0 1))))
+  (exit (if (run-suite '(t.hello-suite)) 0 1)))
 
 (define (flatmap fn ls)
   (apply append (map fn ls)))
@@ -11,8 +10,8 @@
   (let1 result (flatmap run-test mods)
 	(let ((total (length result))
 	      (pass (length (filter (^(x) (eq? x 'ok)) result)))
-	      (err (length (filter (^(x) (not x)) result)))
-	      (ret (fold (^(a b) (and a b)) #t result)))
+	      (err (length (filter (^(x) (eq? x 'err)) result)))
+	      (ret (if (any (^(e) (eq? e 'err)) result) #f #t)))
 	  (print "total: " total ", pass: " pass ", error: " err)
 	  (print (if ret 'ok 'err))
 	  ret)))
