be16b822f589faa1777259ba4b49394581041502
katzchang <katzchang@gmail.com>
2013-02-01 03:22:39 +0900

    refactor

---
+++ gauche/unit/test-suite.scm
@@ -0,0 +1,29 @@
+(define-module gauche.unit.test-suite
+  (export run-suite
+	  run-test)
+
+  (use gauche.sequence)
+  (define (flatmap fn ls)
+    (apply append (map fn ls)))
+  
+  (define (run-suite mods)
+    (let1 result (flatmap run-test mods)
+	  (let ((total (length result))
+		(pass (length (filter (^(x) (eq? x 'ok)) result)))
+		(err (length (filter (^(x) (eq? x 'err)) result)))
+		(ret (if (any (^(e) (eq? e 'err)) result) #f #t)))
+	    (print "total: " total ", pass: " pass ", error: " err)
+	    (print (if ret 'ok 'err))
+	    err)))
+  
+  (define (run-test mod)
+    (let1 result
+	  (eval `(use ,mod) (interaction-environment))
+	  (let1 exports (module-exports (find-module mod))
+		(map (lambda (s)
+		       (print s "...")
+		       (guard (e (else (begin (print e) 'err)))
+			      (begin
+				(eval `(,s) (interaction-environment))
+				'ok)))
+		     exports)))))
