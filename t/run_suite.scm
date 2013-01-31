fde7cf09b70730261613f84c2795d65d4c75c0da
katzchang <katzchang@gmail.com>
2013-01-31 23:43:37 +0900

    確実にうごく

--- t/run_suite.scm
+++ t/run_suite.scm
@@ -5,17 +5,20 @@
 
 (use t.hello-suite)
 
-(let1 mod (find-module 't.hello-suite)
+(exit 
+ (let1 mod (find-module 't.hello-suite)
       (let1 exports (module-exports mod)
 	    (print "tests: " exports)
-	    (for-each
-	     (lambda (s)
-	       (print "running " s)
-	       (guard (e (else (print e)))
-		      (begin
-			(eval `(,s) (interaction-environment))
-			(print 'ok))))
-	     exports)
-	    )
-      )
+	    (if (fold (^(a b) (and a b)) #t
+		      (map
+		       (lambda (s)
+			 (print "running " s)
+			 (guard (e (else (begin (print e) #f)))
+				(begin
+				  (eval `(,s) (interaction-environment))
+				  (print 'ok)
+				  #t)))
+		       exports))
+		0
+		1))))
 
