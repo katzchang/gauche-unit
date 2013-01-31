c2127981b52f93410f34e01b2457eabeccdaf971
katzchang <katzchang@gmail.com>
2013-02-01 02:24:19 +0900

    refactor

--- t/run_suite.scm
+++ t/run_suite.scm
@@ -1,25 +1,30 @@
 (use gauche.sequence)
 
-(use t.hello-suite)
 (define (main args)
-  (let1 result
-	(let1 mod (find-module 't.hello-suite)
-	      (let1 exports (module-exports mod)
-		    (print "tests: " exports)
-		    (map (lambda (s)
-			   (print s "...")
-			   (guard (e (else (begin (print e) 'err)))
-				  (begin
-				    (eval `(,s) (interaction-environment))
-				    'ok)))
-			 exports)))
+  (exit (let1 ret (run-suite '(t.hello-suite))
+	      (if ret 0 1))))
+
+(define (flatmap fn ls)
+  (apply append (map fn ls)))
+
+(define (run-suite mods)
+  (let1 result (flatmap run-test mods)
 	(let ((total (length result))
 	      (pass (length (filter (^(x) (eq? x 'ok)) result)))
 	      (err (length (filter (^(x) (not x)) result)))
 	      (ret (fold (^(a b) (and a b)) #t result)))
 	  (print "total: " total ", pass: " pass ", error: " err)
 	  (print (if ret 'ok 'err))
-	  (exit (if ret 0 1)))))
+	  ret)))
 
-
-
+(define (run-test mod)
+  (let1 result
+	(eval `(use ,mod) (interaction-environment))
+	(let1 exports (module-exports (find-module mod))
+	      (map (lambda (s)
+		     (print s "...")
+		     (guard (e (else (begin (print e) 'err)))
+			    (begin
+			      (eval `(,s) (interaction-environment))
+			      'ok)))
+		   exports))))
