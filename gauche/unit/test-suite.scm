6e951307eb7a3e86f04055a3f19700c935f6dcdb
katzchang <katzchang@gmail.com>
2013-02-01 03:59:37 +0900

    refactor

--- gauche/unit/test-suite.scm
+++ gauche/unit/test-suite.scm
@@ -9,27 +9,29 @@
 
   (define (flatmap fn ls)
     (apply append (map fn ls)))
-  
   (define (collect item ls)
     (filter (^(x) (eq? x item)) ls))
+  (define count-all length)
+  (define (count-if item ls)
+    (length (collect 'ok ls)))
+
   (define (run-suite mods)
     (let1 result (flatmap run-test mods)
-	  (let ((total (length result))
-		(pass (length (collect 'ok result)))
-		(err (length (collect 'err result))))
+	  (let ((total (count-all result))
+		(pass (count-if 'ok result))
+		(err (count-if 'err result)))
 	    (print "total: " total ", pass: " pass ", error: " err)
 	    (print (if (= err 0) 'ok 'err))
 	    err)))
 
   (define env (interaction-environment))
   (define (run-test mod)
-    (let1 result
-	  (eval `(use ,mod) env)
-	  (let1 exports (module-exports (find-module mod))
-		(map (lambda (s)
-		       (print s "...")
-		       (guard (e (else (begin (print e) 'err)))
-			      (begin
-				(eval `(,s) env)
-				'ok)))
-		     exports)))))
+    (eval `(use ,mod) env)
+    (let1 exports (module-exports (find-module mod))
+	  (map (^(s)
+		 (print s "...")
+		 (guard (e (else (begin (print e) 'err)))
+			(begin
+			  (eval `(,s) env)
+			  'ok)))
+	   exports))))
